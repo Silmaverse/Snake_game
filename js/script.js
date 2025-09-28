87f43ea4c05ac91f0668d8eff0631aebe93d7ddd
silma subah <silmasubah105@gmail.com>
2025-09-28 09:31:56 +0600

    changes

--- js/script.js
+++ js/script.js
@@ -6,6 +6,7 @@ let total_col =20;
 let board;
 let context;
 let gameOver = false;
+let gameOverSound = new Audio("../src/gameover.mp3")
 
 //snake coordinates
 let snakeX ;
@@ -18,9 +19,35 @@ let speedY = 0;
 
 //food coordinates
 let foodX, foodY
+let foodeat = new Audio("../src/food.mp3")
 // scorcount;
-let score =document.querySelector("")
+let score =document.querySelector(".score");
+let scorcount=0;
+// button
+let btn =document.querySelector('button');
 let myInterval;
+
+    // level
+   let level = document.querySelector(".gamelevel");
+  // define speeds for each level (ms per update)
+    const speedMap = {
+        "Easy": 500,     // slower
+        "Medium": 200,   // medium
+        "Hard": 100      // fast
+    };
+
+    // function to start interval
+    function startGameInterval(levelValue) {
+        clearInterval(myInterval);
+        let speed = speedMap[levelValue] || 500; // default Easy
+        myInterval = setInterval(update, speed);
+    }
+
+    // listen for level change
+    level.addEventListener("change", function () {
+        startGameInterval(this.value);
+        this.blur();
+    });
 
 window.onload = function () {
 
@@ -41,10 +68,13 @@ window.onload = function () {
     context = board.getContext("2d");
   
     document.addEventListener('keydown', changeDirection);
+
     // place the food
+
     placeFood();
-  
-   myInterval = setInterval(update, 1000/5);
+
+     // start with default level from select
+    startGameInterval(level.value);
     
 }
 
@@ -68,7 +98,10 @@ function update() {
 
 
     if (snakeX === foodX && snakeY === foodY) {
+        scorcount+=1;
+        score.innerHTML=`Score : ${scorcount}`
         snakeBody.push([foodX ,foodY])
+        foodeat.play();
         placeFood();
     }
 
@@ -131,10 +164,15 @@ function update() {
     if (snakeX <0 || snakeX >board.width || snakeY < 0 || snakeY > board.height ) {
       
         gameOver = true;
+        gameOverSound.play();
         let fontsize = Math.floor(board.width / 15);
         context.font = fontsize+"px Arial";
         context.fillStyle = "red";
         context.fillText("Game Over" , board.width /3 ,board.height /3);
+
+        btn.style="visibility :visible";
+        level.style ="visibility :hidden";
+        
     
     }
 
@@ -142,10 +180,14 @@ function update() {
     for (let i = 0; i < snakeBody.length; i++){
         if (snakeX === snakeBody[i][0] && snakeY === snakeBody[i][1]) {
             gameOver = true;
+            gameOverSound.play();
             let fontsize = Math.floor(board.width / 15);
             context.font = fontsize + "px Arial";
             context.fillStyle = "red";
             context.fillText("Game Over", board.width / 3, board.height / 3);
+
+             btn.style="visibility :visible";
+             level.style ="visibility :hidden";
         }
     }
 
@@ -156,26 +198,26 @@ function update() {
 // changing direction of sanke in x and  y
 function changeDirection(e) {
    
-    if (e.code == "ArrowUp") {
+    if (e.code == "ArrowUp" && speedX !=1) {
         speedX = 0;
         speedY = -1;
         
     }
 
-     else if (e.code == "ArrowDown") {
+     else if (e.code == "ArrowDown" && speedY!=-1) {
         speedX = 0;
         speedY = 1;
         
     }
     
-      else if (e.code == "ArrowLeft") {
+      else if (e.code == "ArrowLeft" && speedX !=1) {
         speedX = -1;
         speedY = 0;
         
 
     }
     
-    else if (e.code == "ArrowRight") {
+    else if (e.code == "ArrowRight"  && speedX !=-1) {
         speedX = 1;
         speedY = 0;
        
@@ -187,4 +229,10 @@ function changeDirection(e) {
 function placeFood() {
     foodX = Math.floor(Math.random() * total_col) * blocksize;
     foodY = Math.floor(Math.random() * total_row) * blocksize;
+}
+
+
+// resatrt the game
+function restart(){
+    location.reload();
 }
